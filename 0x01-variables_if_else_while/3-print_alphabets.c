eb9095eb02adbaaa97a15eb4d89a155752789b05
MartinScript <martinsojoola2@gmail.com>
2022-09-09 17:32:33 -0700

    fix bugs

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-    int i, j;
-
-    for (i = 97; i <= 122; i++)
-    {
-        putchar(i);
-    }
-    for (j = 65; j <= 90; j++)
-    {
-        putchar(j);
-    }
-    putchar('\n');
-    return (0);
+	int i, j;
+	
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	for (j = 65; j <= 90; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
 }
