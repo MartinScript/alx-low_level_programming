5202188e0085cef170bd7514030a5826ef896c41
MartinScript <martinsojoola2@gmail.com>
2022-09-09 18:01:35 -0700

    fix bugs

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,13 +6,12 @@
  */
 int main(void)
 {
-    int i;
-
-    for (i = 0; i < 10; i++)
-    {
-        printf("%i", i);
-    }
-    putchar('\n');
-
-    return (0);
+	int i;
+	
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+	return (0);
 }
