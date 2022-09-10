56ee41ac48cdec1953bb466bc318c7c2cd5e985d
MartinScript <martinsojoola2@gmail.com>
2022-09-09 17:50:03 -0700

    fix bugs

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-    for (int i = 97; i <= 122; i++)
-    {
-        if ((i == 101) || (i == 113))
-        {
-            continue;
-        }
-        putchar(i);
-    }
-    putchar('\n');
-    return (0);
+	char c = 'a';
+	
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
 }
