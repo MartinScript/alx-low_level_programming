1f0aceaf825687ba12da65c5f1c620049b5de86e
Martins Ojo <martinsojoola2@gmail.com>
2022-09-10 01:10:30 +0100

    create printable alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Use `putchar` function to print the alphabet in lowercase.
+ * Return: 0
+ */
+int main(void)
+{
+    int i;
+
+    for (i = 97; i <= 122; i++)
+    {
+        putchar(i);
+    }
+    putchar('\n');
+    return (0);
+}
