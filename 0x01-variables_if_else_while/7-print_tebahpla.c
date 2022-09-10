8cb256e5970e3f1802e74edbf826c0dc9579b3ea
Martins Ojo <martinsojoola2@gmail.com>
2022-09-10 02:10:48 +0100

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print all numbers of base 10, starting from 0.
+ * You are not allowed to use any variable of type char.
+ * You can only use `putchar` to print to console.
+ * You can only use `putchar` twice.
+ * Return: 0
+ */
+int main(void)
+{
+    int i;
+
+    for (i = 122; i >= 97; i--)
+    {
+        putchar(i);
+    }
+    putchar('\n');
+    return (0);
+}
