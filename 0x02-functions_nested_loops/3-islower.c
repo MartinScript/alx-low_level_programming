5f2700a1e4d3177a213dc443235641775a7b3c39
MartinScript <martinsojoola2@gmail.com>
2022-09-13 09:39:57 -0700

    define function that checks for lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - checks if alphabet is lowercase
+ *@c: checks for input of functions
+ * Return: return 1 if lowercase else always 0 (success)
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	} else
+		return (0);
