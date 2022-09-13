7eb5507b260d7e214e37a34051c29ed42a3d2843
MartinScript <martinsojoola2@gmail.com>
2022-09-13 09:13:36 -0700

    fix bugs

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,9 @@ void print_alphabet_x10(void)
 	char ch = 'a';
 	int i = 0;
 
-	for (;i < 10;i++)
+	for (i = 0;i < 10;i++)
 	{
-		for (;ch <= 'z';ch++)
+		for (ch = 'a';ch <= 'z';ch++)
 		{
 			_putchar(ch);
 		}
