16acaffd7eb36f8566669a9c4905a5e37a8ed809
MartinScript <martinsojoola2@gmail.com>
2022-09-13 08:07:08 -0700

    fix bugs

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,41 @@
-#include "main.h"
+#include"main.h"
+
+
+
 /**
- * main - Prints putchar
+
+ * main - Entry point
+
  *
- * Return - Always 0 (success)
- */
+
+ * Description: prints _putchar using putchar prototype
+
+ *
+
+ * Return: Always 0 (Success)
+
+*/
+
+
+
 int main(void)
+
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\0');
+
+	char str[] = "_putchar";
+
+	int ch;
+
+
+
+	for (ch = 0; ch < 8; ++ch)
+
+		_putchar(str[ch]);
+
+	_putchar('\n');
+
+
+
+	return (0);
+
 }
