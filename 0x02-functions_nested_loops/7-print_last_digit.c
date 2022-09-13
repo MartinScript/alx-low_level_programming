59ab9fefc60fe8d6a71f85822bda4a096e611de8
MartinScript <martinsojoola2@gmail.com>
2022-09-13 16:03:55 -0700

    define function that returns last digit of a value

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit of a value
+ * @n: checks for function input
+ * Return: returns always 0 (success)
+ */
+int print_last_digit(int n)
+{
+	if (n < 0)
+	{
+		n = n * (-1);
+		n = n % 10;
+		return (n);
+	}
+	else
+	{
+		n = n % 10;
+		return (n);
+	}
+}
