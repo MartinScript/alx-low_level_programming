9f3939e0f15475d81134ab41de5518efbf260f64
MartinScript <martinsojoola2@gmail.com>
2022-09-13 18:23:01 -0700

    define add function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - add two integers
+ * @x: check for first function input
+ * @y: checks for second function input
+ * Return: returns the sum of two integers
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
