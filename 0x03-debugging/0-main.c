3f5ca02aec347bbbb2fde7c30babc14464e5e488
MartinScript <martinsojoola2@gmail.com>
2022-09-15 05:09:50 -0700

    first commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Test for positive or negative numbers.
+ *
+ * Return: return always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
