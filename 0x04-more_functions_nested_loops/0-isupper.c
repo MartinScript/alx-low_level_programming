7d62bc5b072322e2a9f9ec90f26437e65758caea
MartinScript <martinsojoola2@gmail.com>
2022-09-15 16:07:50 -0700

    3 new functions added

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase
+ * @c: checks for function input
+ * Return: return 1 if uppercase else return 0
+ */
+int _isupper(int c);
+{
+	if (c >= 65 && c <=90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
