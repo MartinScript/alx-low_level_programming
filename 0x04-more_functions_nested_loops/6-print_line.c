86a47d2ac3d3937862d5f03b810d506eded06a18
MartinScript <martinsojoola2@gmail.com>
2022-09-15 17:58:04 -0700

    good

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,9 +8,16 @@ void print_line(int n)
 {
 	int = 0;
 
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		_putchar('_');
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	else
+	{
+		_putchar('\n');
 	}
 	_putchar('\n');
 }
