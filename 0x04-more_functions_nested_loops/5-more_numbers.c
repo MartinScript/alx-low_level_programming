3261e4a8b53e8690398db349bf7549d9fb87bf2f
MartinScript <martinsojoola2@gmail.com>
2022-09-15 17:35:22 -0700

    m

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,12 +15,9 @@ void more_numbers(void)
 			if (n > 9)
 			{
 				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
 			}
-			else
-			{
-				_putchar(n + '0');
-			}
+			_putchar((n % 10) + '0');
 		}
+		_putchar('\n');
 	}
 }
