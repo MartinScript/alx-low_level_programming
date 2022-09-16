def9593fcd65a3b2a6eeef21ccc87d76de2fc50e
MartinScript <martinsojoola2@gmail.com>
2022-09-15 18:27:43 -0700

    fix code' git push

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
  * print_square - print square with size of n
- * @n: check for function input
+ * @size: check for function input
  * return: returns void
  */
 void print_square(int size)
 {
 	int i, m;
+
 	m = 1;
 
 	if (n > 0)
 	{
-		for (i = 0; i < (n * n); i++)
+		for (i = 1; i <= (size * size); i++)
 		{
 			_putchar('#');
 			if (i == (n * m))
