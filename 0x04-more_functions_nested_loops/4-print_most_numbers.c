91c76bb532fe63e0a2f1ad32c8fbd76a09d7f0db
MartinScript <martinsojoola2@gmail.com>
2022-09-15 17:01:38 -0700

    fix bugs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,11 +8,11 @@ void print_most_numbers(void)
 {
 	int n = 0;
 
-	while (n < 10)
+	for (n = 0; n < 10; n++)
 	{
-		_putchar(n);
 		if (n == 2 || n == 4)
 			continue;
+		_putchar(n + '0');
 	}
 	_putchar('\n');
 }
