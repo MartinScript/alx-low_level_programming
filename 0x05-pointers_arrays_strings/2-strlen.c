42e82e1170649ac15a20e565332b1491708e37af
MartinScript <martinsojoola2@gmail.com>
2022-09-19 21:13:37 -0700

    fix bugs

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,16 +4,14 @@
  * @s: checks for function input
  * Return: returns length
  */
-
 int _strlen(char *s)
 {
-    int i, sum;
+	int i, sum;
 
-    sum = 0;
-
-    for (i = 0; *(s + i); i++)
-    {
-        sum++;
-    }
-    return (sum);
+	sum = 0;
+	for (i = 0; *(s + i); i++)
+	{
+		sum++;
+	}
+	return (sum);
 }
