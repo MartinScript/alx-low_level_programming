05c89a8f38a23d2375a62511c2996ea6424e0b39
MartinScript <martinsojoola2@gmail.com>
2022-09-20 18:21:42 -0700

    fix bugs

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -5,15 +5,21 @@
  * @n: checks for function input
  * Return: returns void
  */
-
 void print_array(int *a, int n)
 {
-    int i;
-
-    for (i = 0; i < (n - 1); i++)
-    {
-        printf("%d, ", a[i]);
-    }
-    printf("%d", a[n - 1]);
-    printf("\n");
+	int i;
+	
+	if (n > 0)
+	{
+		for (i = 0; i < (n - 1); i++)
+		{
+			printf("%d, ", a[i]);
+		}
+		printf("%d", a[n - 1]);
+		printf("\n");
+	}
+	else
+	{
+		printf("\n\n");
+	}
 }
