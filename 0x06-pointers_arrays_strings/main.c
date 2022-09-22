f92f573bc3fb2dc8ced219920182cd94c52fdc17
Martins Ojo <martinsojoola2@gmail.com>
2022-09-22 11:56:49 +0100

    fix bugs

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -1,7 +1,12 @@
+#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
     char s1[98] = "Hello ";
     char s2[] = "World!\n";
@@ -10,6 +15,8 @@ int main()
     printf("%s\n", s1);
     printf("%s", s2);
     ptr = _strcat(s1, s2);
+    printf("%s", s1);
+    printf("%s", s2);
     printf("%s", ptr);
-    return 0;
+    return (0);
 }
