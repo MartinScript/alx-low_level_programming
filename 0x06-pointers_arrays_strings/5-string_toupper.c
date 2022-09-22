6eac1e01df78f78aec2bf31b7e844d8a7b39e7ca
Martins Ojo <martinsojoola2@gmail.com>
2022-09-23 00:02:38 +0100

    correct indentation

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,17 +7,18 @@
 
 char *string_toupper(char *str)
 {
-    int i;
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        if (str[i] >= 'a' && str[i] <= 'z')
-        {
-            str[i] = str[i] - 32;
-        }
-        else
-        {
-            str[i] = str[i];
-        }
-    }
-    return (str);
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		else
+		{
+			str[i] = str[i];
+		}
+	}
+	return (str);
 }
