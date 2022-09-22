7c59e3a6eac8f1220088f6b86435b60b5d0adf9b
Martins Ojo <martinsojoola2@gmail.com>
2022-09-22 11:01:29 +0100

    first commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+char *_strcat(char *dest, char *src)
+{
+    int i, j;
+	char *ptr_str;
+	ptr_str = malloc(strlen(dest) + strlen(src) + 2);
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		ptr_str[i] = dest[i];
+
+	}
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		ptr_str[i + j] = src[j];
+
+	}
+	ptr_str[i + j] = '\0';
+	return (ptr_str);
+
+}
---
+++ 0x06-pointers_arrays_strings/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
+
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = _strcat(s1, s2);
+    printf("%s", ptr);
+    return 0;
+}
