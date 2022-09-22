9876c1ff5273e76fba2a8fbf0dd5a9688ed99fef
Martins Ojo <martinsojoola2@gmail.com>
2022-09-22 17:52:16 +0100

    fix bugs

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,12 +8,14 @@
 void reverse_array(int *a, int n)
 {
 	int i, j;
-	int arr1[n], arr2[n];
+	int *arr1, *arr2;
+	arr1 = malloc(sizeof(int) * n);
+	arr2 = malloc(sizeof(int) * n);
 
 	for (i = 0; i < n; i++)
 	{
-		arr1[i] = a[i];
-		arr2[i] = a[n - i - 1];
+		*(arr1 + i) = a[i];
+		*(arr2 + i) = a[n - i - 1];
 	}
 	for (j = 0; j < n; j++)
 	{
