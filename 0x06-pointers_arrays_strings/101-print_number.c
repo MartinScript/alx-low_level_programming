753ac615e4a149876d11be54d8f67c1b93dcd470
Martins Ojo <martinsojoola2@gmail.com>
2022-09-23 02:00:30 +0100

    fix indentations

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,6 +3,7 @@
 /**
 * print_number - Prints the number given.
 * @n: number.
+* Return: returns void
 */
 void print_number(int n)
 {
@@ -11,18 +12,14 @@ void print_number(int n)
 		n *= -1;
 		_putchar ('-');
 	}
-
 	if (n > 9999)
 		_putchar (n % 100000 / 10000 + '0');
 
 	if (n > 999)
 		_putchar (n % 10000 / 1000 + '0');
-
 	if (n > 99)
 		_putchar (n % 1000 / 100 + '0');
-
 	if (n > 9)
 		_putchar (n % 100 / 10 + '0');
-
 	_putchar (n % 10 + '0');
 }
