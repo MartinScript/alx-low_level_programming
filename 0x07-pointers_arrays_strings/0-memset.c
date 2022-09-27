28f8a1e2ff4314d8481e22b282f683c31db2c7da
MartinScript <martinsojoola2@gmail.com>
2022-09-26 18:00:43 -0700

    second commit'

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - function that fills memory with a constant byte.
+ *@s: function input.
+ *@b: function input
+ *@n: function input
+ *Return: char
+ **/
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	unsigned char *p = s;
+
+	while (n--)
+	{
+		*p++ = (unsigned char)b;
+	}
+	return (s);
+}
