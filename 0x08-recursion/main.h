41c155af736e7210975ec19a2b33a51e64461d80
Martins Ojo <martinsojoola2@gmail.com>
2022-09-28 17:54:11 +0100

    define is_prime

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,6 +3,8 @@
 
 int _putchar(char c);
 int halp(int c, int i);
+int _sqrt(int x, int i);
+int hai(int n, int d);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
