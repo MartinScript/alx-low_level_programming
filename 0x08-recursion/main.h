9555f25b71c97897812764d111dd1719e58500eb
Martins Ojo <martinsojoola2@gmail.com>
2022-09-28 18:03:15 +0100

    last commit

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,6 +7,7 @@ int _sqrt(int x, int i);
 int hai(int n, int d);
 int my_pal(char *s, int l);
 int get_length(char *s);
+int findsrc(char *s, char c, int i, int p);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
