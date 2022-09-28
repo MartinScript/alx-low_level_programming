8beb10e75d7928124bcceda8c9d688b7541c2bcd
Martins Ojo <martinsojoola2@gmail.com>
2022-09-28 14:54:06 +0100

    first commit

---
+++ 0x08-recursion/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    #include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    _puts_recursion("Puts with recursion");
+    return (0);
+}
+}
