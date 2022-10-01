ed4cca58dccbee0873a51b045fae71589e7e0502
Martins Ojo <martinsojoola2@gmail.com>
2022-10-01 01:09:48 +0100

    fix bugs

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,33 +1,74 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * main - multiply 2 numbers passed to main, or Error
- * @argc: argument count
- * @argv: argument vector
- * Return: 1 if error, 0 if function runs correctly
- */
-int main(int argc, char *argv[])
-{
-	int i, sum;
-	sum = 0;
 
-	if (argv[i] >= '0' && argv[i] <= '9')
+/**
+ * _isdigit - Returns weather character is a digit or not.
+ * @c: charater fed in.
+ * Return: 1 or 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
 	{
-		for (i = 1; i < argc; i++)
-		{
-			sum = sum + atoi(argv[i]);
-		}
-		printf("%d\n", sum);
 		return (0);
-	}
-	else if (argc = 1)
-	{
-		printf("0\n");
 	}
 	else
 	{
-		printf("Error\n");
 		return (1);
 	}
+}
+
+/**
+ * char_check - Checks every char of input string to see if it's an int.
+ * @s: Passed in string.
+ * Return: 1 if char is found; 0 if only ints.
+ */
+
+int char_check(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (_isdigit(s[i]) == 1)
+			return (1);
+	}
+
+	return (0);
+}
+
+/**
+ * main - Adds numbers passed it.
+ * @argc: Number of arguments passed.
+ * @argv: Array of arguments passed.
+ * Return: 1 if int found; 0 if flawless execution is achived.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, a, res;
+
+	res = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (char_check(argv[i]) == 1)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		a = atoi(argv[i]);
+		res += a;
+	}
+
+	printf("%d\n", res);
+	return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,5 +2,7 @@
 #define MAIN_H_
 
 int _putchar(char c);
+int _isdigit(int c);
+int char_check(char *s);
 
 #endif /*MAIN_H_*/
