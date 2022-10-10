0c05adde7c0362a34c358625d5f320d9abfe7e3b
Martins Ojo <martinsojoola2@gmail.com>
2022-10-10 23:56:50 +0100

    fix bugs

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
-#include "dog.h"
 #include <stdio.h>
+#include "dog.h"
 /**
  ** init_dog - initialiazes a dog structure.
  ** @d: pointer to dog structure.
@@ -7,21 +7,14 @@
  ** @age: age of dog.
  ** @owner: dog's owner.
  **/
-int main(void)
-{
-	return (0);
-}
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 
 {
 
-		d->name = name;
+	d->name = name;
 
-			d->age = age;
+	d->age = age;
 
-				d->owner = owner;
-
+	d->owner = owner;
 };
-
-
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H_INCLUDED
-#define DOG_H_INCLUDED
+#ifndef DOG
+#define DOG
 #include <stdlib.h>
 /**
  * struct dog- a structure of dog.
@@ -19,6 +19,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
-#endif /*DOG_H_INCLUDED*/
-
+#endif
