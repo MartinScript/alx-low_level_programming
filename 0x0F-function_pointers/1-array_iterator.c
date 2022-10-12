4562c78ff09e9150293e72df988bd57c6bd0c37d
MartinScript <martinsojoola2@gmail.com>
2022-10-12 11:17:56 -0700

    define array_iterator function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * array_iterator - a function that executes a function given as
+ * 		 a parameter on each element of an array.
+ * @array: array.
+ * @size: size of array.
+ * @action: pointer to function.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (size != 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
+
