a38da71a567b383e9fd07f0d3fd5c92dfc38faaa
MartinScript <martinsojoola2@gmail.com>
2022-10-12 15:47:44 -0700

    define int_index function

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include  "function_pointers.h"
+#include <stddef.h>
+/**
+ * int_index - a function that searches for an integer.
+ * @array: array.
+ * @size: size of array.
+ * @cmp: pointer to function.
+ * Return: returns 0 (success), returns -1 (failure).
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return;
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				break;
+			else
+				return (-1);
+		}
+		return (i);
+	}
+}
+
