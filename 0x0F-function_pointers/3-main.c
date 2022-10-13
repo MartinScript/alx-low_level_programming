b312a7b6b9157f06a5a5d63041bcec3772821174
MartinScript <martinsojoola2@gmail.com>
2022-10-12 17:38:45 -0700

    define working calculator

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,44 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Calculator, that can perform addition, subtraction,
+ * multiplication, division, and modulus.
+ * Takes input from standard in, and prints to standard out.
+ * @argc: Number of arguments passed in from standard input.
+ * @argv: Array of strings passed in from standard input.
+ * Return: 0.
+ * Exit: Codes for errors caused by missuse of program.
+ * 98 - Incorrect number of arguments to program.
+ * 99 - Oporator not found.
+ * 100 - STOP DIVIDING BY ZERO!
+ **/
+int main(int argc, char *argv[])
+{
+	int (*calc)(int a, int b);
+	int (*get_op_func(char *s))(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	calc = get_op_func(argv[2]);
+
+	if ((calc == op_div || calc == op_mod) && atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	if (calc == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", calc(atoi(argv[1]), atoi(argv[3])));
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "3-calc.h"
+#include <stdlib.h>
+/**
+ * op_add - add an integer to another
+ * @a: integer a
+ * @b: integer b
+ * Return - sum of a and b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substract one integer from another
+ * @a: integer a
+ * @b: integer b
+ * Return - returns the difference of a and b.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply two integers
+ * @a: integer a
+ * @b: integer b
+ * Return - returns the product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b)
+}
+
+/**
+ * op_div - divide an integer by another integer
+ * @a: integer a
+ * @b: integer b
+ * Return - returns the result of the division of a by b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - calculate the remainder of a divided by b
+ * @a: integer a
+ * @b: integer b
+ * Return - returns the remainder of the division of a by b
+ */
+Prototype: int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
