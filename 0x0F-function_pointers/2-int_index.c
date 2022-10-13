d35f1dfce35fb9c90e56d2a86eb33f5aeae13523
MartinScript <martinsojoola2@gmail.com>
2022-10-12 18:29:26 -0700

    last commit

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,14 +13,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (array == NULL || cmp == NULL || size <= 0)
 		return (-1);
-	else
-	{
-		for (i = 0; i < size; i++)
+
+	for (i = 0; i < size; i++)
 		{
 			if (cmp(array[i]))
 				return (i);
 		}
-		return (-1);
-	}
+	return (-1);
 }
 
