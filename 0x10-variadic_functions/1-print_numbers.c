4cc7b57efbe0411d21a1b8ea922451030d1e4b47
Martins Ojo <martinsojoola2@gmail.com>
2022-10-14 01:02:46 +0100

    define print_numbers function

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdlib.h>
+/**
+ * print_numbers - a function that prints numbers, followed by a new line.
+ * @separator: character to separate numbers
+ * @n: length of numbers to print.
+ * @...: variable length parameters.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list argList;
+	unsigned int i;
+
+	if (*separator == NULL)
+		return (NULL);
+
+	for (i = 0; i < (n - 1); i++)
+		printf("%d%s", va_arg(argList, int), separator);
+	printf("%d\n", va_arg(argList, int));
+	va_end(argList);
+}
