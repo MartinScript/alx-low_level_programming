1a66ecaceb21f59a01780512796fe8fa6d958458
Martins Ojo <martinsojoola2@gmail.com>
2023-01-06 03:04:03 +0100

    fix bugs

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,6 +13,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *ptr;
 	index = key_index((const unsigned char *)key, ht->size);
+
 	if (ht == NULL || key == NULL)
 	{
 		return (NULL);
--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -4,7 +4,7 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print - Print a hash table
+ * hash_table_delete - delete hash table
  * @ht: hash table
  */
 void hash_table_delete(hash_table_t *ht)
